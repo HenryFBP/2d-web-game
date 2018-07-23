a75d8f38befcd5b9f6ceb5b467b3fd48744698d5
HenryFBP <HenryFBP@gmail.com>
2018-07-23 08:05:18 -0400

    migrate all content from https://github.com/HenryFBP/wyzant/

---
+++ js/block.js
@@ -0,0 +1,5 @@
+var Block = /** @class */ (function () {
+    function Block() {
+    }
+    return Block;
+}());
---
+++ js/world.js
@@ -0,0 +1,11 @@
+/// <reference path="../defs/phaser.d.ts" /> 
+// world.ts
+var World = /** @class */ (function () {
+    function World(g, width, height) {
+        this.game = g;
+        this.grid = [];
+    }
+    World.prototype.drawline = function (block, x, y) {
+    };
+    return World;
+}());
